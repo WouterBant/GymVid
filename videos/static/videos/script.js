31e2a33c22a38cc0f383e44eeb994bd7aec6a785
WouterBant <99251015+WouterBant@users.noreply.github.com>
2023-07-12 09:54:17 +0200

    Ligt/dark mode improved

--- videos/static/videos/script.js
+++ videos/static/videos/script.js
@@ -1,34 +1,33 @@
-// This script makes it possible to toggle between light-mode and dark-mode
-
 var icon = document.getElementById("icon");
 
 // Changes the icon and theme when clicked on the symbol
-icon.onclick = function()
-{
-    document.body.classList.toggle("dark-theme");
-
-    if (document.body.classList.contains("dark-theme"))
-    {
-        localStorage.setItem('dark-theme', "no");
-        icon.src = moon;
-    }
-    else
-    {
-        localStorage.setItem('dark-theme', "yes");
-        icon.src = sun;
-    }
+icon.onclick = function() {
+  document.body.classList.toggle("dark-theme");
+  
+  if (document.body.classList.contains("dark-theme")) {
+    localStorage.setItem('dark-theme', "yes");
+    icon.src = moon;
+  } else {
+    localStorage.setItem('dark-theme', "no");
+    icon.src = sun;
+  }
 }
 
-// On load check if the user selected light-mode, if so switch theme (default is dark)
-function load1()
-{
-    if (localStorage.getItem('dark-theme'))
-    {
-        const no_toggle_needed = localStorage.getItem('dark-theme');
-        if (no_toggle_needed == "no")
-        {
-            document.body.classList.toggle("dark-theme");
-            icon.src = moon;
-        }
-    }
+// On load, check if the user selected a mode, if not, use the default mode
+function loadTheme() {
+  var userPreference = localStorage.getItem('dark-theme');
+  
+  if (userPreference === null) {
+    var prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    userPreference = prefersDarkMode ? "yes" : "no";
+    localStorage.setItem('dark-theme', userPreference);
+  }
+  
+  if (userPreference === "yes") {
+    document.body.classList.add("dark-theme");
+    icon.src = moon;
+  }
 }
+
+// Call the loadTheme function when the page finishes loading
+window.addEventListener('load', loadTheme);
